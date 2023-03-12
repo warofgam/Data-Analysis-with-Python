9fc212185c50651e881fbfe164aa736ddd218fe1
warofgam <127661194+warofgam@users.noreply.github.com>
2023-03-12 13:38:45 +0300

    Update submission_template01.py

--- Deep Learning/submission_template01.py
+++ Deep Learning/submission_template01.py
@@ -3,13 +3,20 @@ import torch
 from torch import nn
 
 def create_model():
-    # your code here
-    # return model instance (None is just a placeholder)
-
-    return None
+    NN = nn.Sequential(nn.Linear(784, 256, bias=True),
+                   nn.ReLU(),
+                   nn.Linear(256, 16, bias=True),
+                   nn.ReLU(),
+                   nn.Linear(16, 10, bias=True),
+                   nn.ReLU())
+    return NN
 
 def count_parameters(model):
-    # your code here
-    # return integer number (None is just a placeholder)
-    
-    return None
+  sum = 0
+  for param in model.parameters():
+    sum1 = 1
+    for i in range(len(param.shape)):
+      sum1 *= param.shape[i]
+    sum += sum1
+
+  return sum
